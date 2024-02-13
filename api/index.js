43968860cac898bcc21562306b5490ae07f4cf3a
mohit <mohitb12363@gamil.com>
2024-02-13 20:25:03 +0530

    create sign up api route and connect with database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 const app = express();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -9,6 +11,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err.message)
 })
 
+app.use(express.json());
+
 app.listen(process.env.PORT || 5000,()=>{
     console.log(`Server is running on port ${process.env.PORT}!`);
 })
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
