dd29f2186d0e44848c11f1971c7bff1959fe5337
mohit <mohitb12363@gamil.com>
2024-02-14 19:44:32 +0530

    Complete sign-in page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold m-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="num"
+          placeholder="Enter Your Number"
+          id="number"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 to-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          Sign up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>
+          Have an account.. ?
+          <Link to="/sign-in">
+          <span className="text-blue-500">Sign in</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
