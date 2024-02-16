11da3f2ac2bdd4556dc89d6e46dec0b03e2129c2
mohit <mohitb12363@gamil.com>
2024-02-16 13:22:16 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-
+import { errorHandler } from "../utlis/error.js";
+import jwt from "jsonwebtoken";
+// import cookie from 'cookie-parser';
 export const signup = async (req, res, next) => {
   const { username, email, number, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -18,4 +20,28 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+        return next(errorHandler(401, "User not found"));
+      }
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) {
+        return next(errorHandler(401, "Invalid password"));
+      }
+      const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+      // for hidding password like we dont want to give and show password to the use 
+      const{password: hashedPassword, ...rest} = validUser._doc;
+      const expiryDate = new Date(Date.now() +360000);
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: expiryDate
+      }).status(200).json(rest)
+    } catch (error) {
+      next(error);
+    }
 };
